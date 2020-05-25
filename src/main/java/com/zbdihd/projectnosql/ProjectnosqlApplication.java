8f09d1d0aee1b343eff2fe41525708fe2974ebad
John <jasieka@interia.eu>
2020-05-25 22:21:05 +0200

    Create project

---
+++ src/main/java/com/zbdihd/projectnosql/ProjectnosqlApplication.java
@@ -0,0 +1,13 @@
+package com.zbdihd.projectnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zbdihd/projectnosql/ProjectnosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbdihd.projectnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectnosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
